58e7355782df8d45ab93c39ca11ee6ef51d859b6
Tirsa St. Fort <lifelovelori@gmail.com>
2022-12-12 21:05:12 -0500

    commit tree 61b2bdfd3270eaa7dc2adf379ec5f971f5e1a128

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph")
+} 
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+} 
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+} 
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+} 
+
+function appendCat(name) {
+    const appendCat = [...cats, "Broom"]
+    return appendCat
+} 
+
+function prependCat(name) {
+    const prependCat = ["Arnold", ...cats]
+    return prependCat
+} 
+
+function removeLastCat() {
+    removeLastCat = cats.slice(0,cats.length - 1)
+    return removeLastCat
+} 
+
+function removeFirstCat() {
+    removeFirstCat = cats.slice(1)
+    return removeFirstCat
+} 
